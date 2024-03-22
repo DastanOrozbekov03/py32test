eedfa8c2521e87444741d61af11585ff265b0791
Machoooooo8888 <157127232+Machoooooo8888@users.noreply.github.com>
2024-03-22 20:59:12 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-prinnt = 'hello'
-print('hellooo')
-kjalhkdsgj
-bbbbbbbbbbbbbbb
-'Monkey_D_Luffy'9000000000
-80000000 sdfghjklkjhg
-ilgiz-krutoi
-rsedtyfguhjkl; 
-ilgiz
-000000000000000000000000
-asdsadsadsaddsad
-sadsadsadsa
-sdasdasdasd
-nanaosd
-203240
+asdsadsa
+800000000
+sadsadsad
+bbbbbb
