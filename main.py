fedc0ffb1b030062c8341dec4648f1198be7b2bc
Machoooooo8888 <157127232+Machoooooo8888@users.noreply.github.com>
2024-03-22 21:22:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-dscdwwdw
-cdwwcdwcwdc
-cdwwwwwwwwcdw
-
-
-
-askfjhAKJFH
-StopAsyncIterationSADJKFGSJ
-JASF
-=======
-bbbbbbbbbbbbbbbbb
-5tb5btbt5b55
-5gb5tbt5b5tt5b
->>>>>>> b59b055609882fba86f39d2589a0b137b46b7a5e
+sadasdasdasdasda
+assadasdasdasdasdas
+bbbbbbbbbbbb
+aaaaaaaaaaaaaaa
