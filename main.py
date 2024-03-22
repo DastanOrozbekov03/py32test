815341e4b13b0dd923095844c80a12ec98b55ce2
Machoooooo8888 <157127232+Machoooooo8888@users.noreply.github.com>
2024-03-22 20:56:17 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-prinnt = 'hello'
-print('hellooo')
-kjalhkdsgj
-bbbbbbbbbbbbbbb
-'Monkey_D_Luffy'9000000000
-80000000
-ilgiz-krutoi
-rsedtyfguhjkl; 
-ilgiz
-000000000000000000000000
-asdsadsadsaddsad
+sadsadsadsa
+sdasdasdasd
+nanaosd
+203240
